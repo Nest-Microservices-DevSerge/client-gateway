6e7eb5b60a3ceda954ea41dc7bde3b3769f217af
sergeavila7 <serge_avila7@yahoo.com>
2025-01-31 17:20:33 -0600

    feat(core): add products microservice

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
-import { ProductsModule } from './products/products.module';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ProductsController } from './products/products.controller';
+import { PRODUCT_SERVICE } from './config/services';
+import { envs } from './config';
 
 @Module({
-  imports: [ProductsModule],
-  controllers: [],
+  controllers: [ProductsController],
   providers: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: PRODUCT_SERVICE,
+        transport: Transport.TCP,
+        options: {
+          host: envs.productsMicroserviceHost,
+          port: envs.productsMicroservicePort,
+        },
+      },
+    ]),
+  ],
 })
 export class AppModule {}
