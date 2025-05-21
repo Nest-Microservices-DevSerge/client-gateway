dc87fc9982faeb1d63eac0866a33f7868ef3c15c
sergeavila7 <serge_avila7@yahoo.com>
2025-05-21 00:15:39 -0600

    feat(module) add nats module and delete imports

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,19 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ClientsModule, Transport } from '@nestjs/microservices';
 import { OrdersController } from './orders.controller';
-import { ORDER_SERVICE } from 'src/config/services';
+import { NatsModule } from 'src/transports/nats.module';
 
 @Module({
   controllers: [OrdersController],
   providers: [],
-  imports: [
-    ClientsModule.register([
-      {
-        name: ORDER_SERVICE,
-        transport: Transport.TCP,
-        options: {},
-      },
-    ]),
-  ],
+  imports: [NatsModule],
 })
 export class OrdersModule {}
--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ProductsController } from './products.controller';
-import { ClientsModule, Transport } from '@nestjs/microservices';
-import { NATS_SERVICE, envs } from 'src/config';
 import { NatsModule } from 'src/transports/nats.module';
 
 @Module({
